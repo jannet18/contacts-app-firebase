27b35ad1549c4c54731e48087585d2e871c3d57e
janet <janetngei18@gmail.com>
2023-02-24 16:31:25 +0300

    firebase,components and pages initialization

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import AddEdit from './pages/AddEdit';
+import About from './pages/About';
+import View from './pages/View';
+
+function App() {
+  return (
+    <>
+    <Router>
+    <Routes>
+      <Route exact path='/' element={<Home/>}></Route>
+      <Route path='/add' element={<AddEdit/>}></Route>
+      <Route path='/update:id' element={<AddEdit/>}></Route>
+      <Route path='/view:id' element={<View/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+    </Routes>  
+    </Router>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAXiueMjAhRPbWx9wpt3g352QJjPjHMcLY",
+  authDomain: "contacts-app-c1529.firebaseapp.com",
+  projectId: "contacts-app-c1529",
+  storageBucket: "contacts-app-c1529.appspot.com",
+  messagingSenderId: "110095549272",
+  appId: "1:110095549272:web:6f09a98a7a021206425b2d",
+  measurementId: "G-T09NP33TYL"
+};
+
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
