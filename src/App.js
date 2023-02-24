7968bbc5aa12d541b9e828aad44b9ccf7d299f93
janet <janetngei18@gmail.com>
2023-02-24 17:08:01 +0300

    header elements

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Home from './pages/Home';
 import AddEdit from './pages/AddEdit';
 import About from './pages/About';
 import View from './pages/View';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
     <Router>
+      <Header/>
     <Routes>
       <Route exact path='/' element={<Home/>}></Route>
       <Route path='/add' element={<AddEdit/>}></Route>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
 
 const Header = () => {
+    const [activeTab, setActiveTab] = useState("Home")
   return (
-    <div>Header</div>
+    <div className='header'>
+        <p className='logo'>Contact App</p>
+        <div className='header-right'>
+            <Link to='/'>
+             <p className={`${activeTab === "Home" ? "active" : ""}`} onClick={() => setActiveTab("Home")}>Home</p>   
+            </Link>
+            <Link to='/add'>
+             <p className={`${activeTab === "Add Contact" ? "active" : ""}`} onClick={() => setActiveTab("Add Contact")}>Add Contact</p>   
+            </Link>
+            <Link to='/about'>
+             <p className={`${activeTab === "About" ? "active" : ""}`} onClick={() => setActiveTab("About")}>About</p>   
+            </Link>
+        </div>
+
+    </div>
   )
 }
 
